8ccd3969810ab2a8ca4f30f84af02f5a2b95b3aa
666Dancing666 <Dancing_666@outlook.com>
2026-07-22 19:48:12 +0800

    Fix TileHelper broken by sed

--- tshock-plugin/Utils/TileHelper.cs
+++ tshock-plugin/Utils/TileHelper.cs
@@ -4,44 +4,28 @@ namespace TerrariaBridge.Utils
 {
     public static class TileHelper
     {
-        public static void BreakTile(int x, int y, )
+        public static void BreakTile(int x, int y)
         {
             if (!WorldGen.InWorld(x, y)) return;
-            WorldGen.KillTile(x, y, false, false, false);
+            WorldGen.KillTile(x, y);
             if (Main.netMode == 2)
-            {
                 NetMessage.SendTileSquare(-1, x, y, 1);
-            }
         }
 
-        public static void PlaceTile(int x, int y, int tileType, )
+        public static void PlaceTile(int x, int y, int tileType)
         {
             if (!WorldGen.InWorld(x, y)) return;
-            WorldGen.PlaceTile(x, y, tileType, false, true, playerId);
+            WorldGen.PlaceTile(x, y, tileType, true, true);
             if (Main.netMode == 2)
-            {
                 NetMessage.SendTileSquare(-1, x, y, 1);
-            }
         }
 
-        public static void PlaceWall(int x, int y, int wallType, )
+        public static void PlaceWall(int x, int y, int wallType)
         {
             if (!WorldGen.InWorld(x, y)) return;
-            WorldGen.PlaceWall(x, y, wallType, false);
+            WorldGen.PlaceWall(x, y, wallType, true);
             if (Main.netMode == 2)
-            {
                 NetMessage.SendTileSquare(-1, x, y, 1);
-            }
-        }
-
-        public static bool IsTileBlocked(int x, int y)
-        {
-            if (!WorldGen.InWorld(x, y)) return true;
-            ITile itile = Main.tile[x, y];
-            if (itile == null) return true;
-            Tile tile = (Tile)itile;
-            if (tile.active() && Main.tileSolid[tile.type]) return true;
-            return false;
         }
     }
 }
